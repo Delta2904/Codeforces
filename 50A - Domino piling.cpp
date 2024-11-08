064feea289080eb90873225c009b3ac93ad1912c
Emiliano <2904.emiliano@gmail.com>
2024-11-08 16:50:15 -0600

    Added solution to problem 50A in codeforces

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n, int m) {
+    int max = n * m;
+
+    cout << max / 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    solve(n, m);
+    
+    return 0;
+}
