acc37dcaa6ef265bb86c023a58fbaf5c9cbc6c11
Emiliano <2904.emiliano@gmail.com>
2024-12-04 10:44:47 -0600

    Worked on a simplified solution for problem 266B

--- 266B - Queue at the School.cpp
+++ 266B - Queue at the School.cpp
@@ -6,10 +6,9 @@ void solve(int n, int t) {
     cin >> s;
 
     for(int i = 0; i < t; i++) {
-        for (int j = 0; j < s.size(); j++) {
-            if (s[j] == 'B' && s[j + 1] == 'G' && j <= s.size() - 1) {
-                s[j] = 'G';
-                s[j + 1] = 'B';
+        for (int j = 0; j < n - 1; j++) {
+            if (s[j] == 'B' && s[j + 1] == 'G') {
+                swap(s[j], s[j + 1]);
                 j++;
             }
         }
