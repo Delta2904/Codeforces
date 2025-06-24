aad15210b4ce87f8f2801331b098ac93c0b66ec2
Emiliano <emiliano@pc-bd-ecg.fi.unam>
2025-06-24 13:49:32 -0600

    Added solution to problem 1807A

---
+++ 1807A - Plus or Minus.cpp
@@ -0,0 +1,36 @@
+// Codeforces | ThemeCP Training
+//   /\_/\      Author: Emiliano Calderón (MangoFI)
+//  ( o.o )     Github: github.com/Delta2904
+//   > ^ <      Codeforces: codeforces.com/profile/MangoFI
+//
+// ThemeCP Level: ThemeCP Level 1 - Random
+// Tags         : implementation/*800
+// Problem      : 1807A - Plus or Minus (https://codeforces.com/problemset/problem/1807/A)
+// Date         : 24-06-2025
+// 
+// "We are stardust trying to understand the cosmos,
+//  with the patience and agility of a cat in the night."
+//     — Inspired by Carl Sagan & feline wisdom
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a, b, c; cin >> a >> b >> c;
+    
+    if (a + b == c) cout << "+\n";
+    else cout << "-\n";
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
