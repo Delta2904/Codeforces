7b648c0c9b5352b199e01a6e1b02acf1c22caa5a
Emiliano <2904.emiliano@gmail.com>
2026-04-16 00:13:03 -0600

    Moving working directory and actual branch to the correct enviroment

---
+++ 868B - Race Against Time.cpp
@@ -0,0 +1,32 @@
+// Codeforces | ThemeCP Training
+//   /\_/\      Author: Emiliano Calderón (MangoFI)
+//  ( o.o )     Github: github.com/Delta2904
+//   > ^ <      Codeforces: codeforces.com/profile/MangoFI
+//
+// ThemeCP Level: Training
+// Tags         : brute force/implementation/strings/*900
+// Problem      : 868B - Race Against Time (https://codeforces.com/problemset/problem/868/B)
+// Date         : 06-01-2026
+// 
+// "We are stardust trying to understand the cosmos,
+//  with the patience and agility of a cat in the night."
+//     — Inspired by Carl Sagan & feline wisdom
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    //int t; cin >> t;
+    //while (t--) solve();
+
+    solve();
+
+    return 0;
+}
